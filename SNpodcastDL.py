6037bb831016c3d02e1ce28c71207db21f4259ce
rialbblair <blair.saunders25@gmail.com>
2017-06-24 20:28:42 -0400

    Add files via upload

---
+++ SNpodcastDL.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+import urllib
+
+epnum = raw_input('Episode Number? ')
+baseurl = ('https://media.grc.com/sn/sn-' + epnum + '.mp3')
+downloading = 'Downloading please wait...'
+filename = r'D:\amd8320\Music\Security Now\sn' + epnum + '.mp3'
+
+
+print downloading
+urllib.urlretrieve(baseurl, filename)
+
+
+
+
+
+#setup to dl in a range of episodes
+
+
+
+
+
+    
+
+
+
+
+
